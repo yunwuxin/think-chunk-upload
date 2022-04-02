c18f1ef293bfb83ec47f25ddc617b73bcb2a9310
yunwuxin <448901948@qq.com>
2022-04-02 14:27:45 +0800

    init

--- src/Server.php
+++ src/Server.php
@@ -3,7 +3,8 @@
 namespace think\ChunkUpload;
 
 use Ramsey\Uuid\Uuid;
-use Spatie\TemporaryDirectory\TemporaryDirectory;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
 use think\exception\HttpException;
 use think\File;
 use think\Request;
@@ -15,6 +16,14 @@ class Server
 
     protected $onComplete;
 
+    /** @var Filesystem */
+    protected $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
     public function serve(Request $request, $onComplete)
     {
         $this->request    = $request;
@@ -22,12 +31,31 @@ class Server
 
         $stage = $request->header('x-stage');
 
-        return $this->{$stage}();
+        if (empty($stage)) {
+            return $this->single();
+        } else {
+            return $this->{$stage}();
+        }
+    }
+
+    protected function single()
+    {
+        $filename = Path::join(sys_get_temp_dir(), Uuid::uuid4()->toString());
+
+        $content = $this->request->getContent();
+
+        $this->filesystem->dumpFile($filename, $content);
+
+        if ($this->onComplete) {
+            call_user_func($this->onComplete, new File($filename));
+        }
+
+        return response('', 204);
     }
 
     protected function initiate()
     {
-        $id = $this->createTemporaryDirectory();
+        $id = Uuid::uuid4()->toString();
 
         return response('', 201)->header([
             'x-id' => $id,
@@ -47,7 +75,7 @@ class Server
         $content = $this->request->getContent();
         $sha1    = sha1($content);
 
-        file_put_contents($dir->path("part-{$index}-{$sha1}"), $content);
+        $this->filesystem->dumpFile(Path::join($dir, "part-{$index}-{$sha1}"), $content);
 
         return response()->header([
             'ETag' => $sha1,
@@ -63,12 +91,14 @@ class Server
             throw new HttpException(400);
         }
 
-        $source = $dir->path('file');
-        $fp     = fopen($source, 'w');
+        $dest = Path::join($dir, "file");
+
+        $fp = fopen($dest, 'w');
+
         try {
             foreach ($parts as $part) {
-                $filename = $dir->path("part-{$part['index']}-{$part['etag']}");
-                if (!file_exists($filename)) {
+                $filename = Path::join($dir, "part-{$part['index']}-{$part['etag']}");
+                if (!$this->filesystem->exists($filename)) {
                     abort(400, 'InvalidPart');
                 }
                 $content = file_get_contents($filename);
@@ -76,26 +106,14 @@ class Server
             }
 
             if ($this->onComplete) {
-                call_user_func($this->onComplete, new File($source));
+                call_user_func($this->onComplete, new File($dest));
             }
 
             return response('', 204);
         } finally {
             fclose($fp);
-            $dir->delete();
+            $this->filesystem->remove($dir);
         }
-    }
-
-    protected function createTemporaryDirectory()
-    {
-        $id = Uuid::uuid4()->toString();
-
-        (new TemporaryDirectory())
-            ->name($id)
-            ->force()
-            ->create();
-
-        return $id;
     }
 
     protected function getTemporaryDirectory()
@@ -104,10 +122,10 @@ class Server
         if (empty($id)) {
             throw new HttpException(400);
         }
-        $dir = (new TemporaryDirectory())
-            ->name($id);
 
-        if (!file_exists($dir->path())) {
+        $dir = Path::join(sys_get_temp_dir(), $id);
+
+        if (!$this->filesystem->exists($dir)) {
             throw new HttpException(400);
         }
 
